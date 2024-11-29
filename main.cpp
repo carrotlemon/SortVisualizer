051acb83b7cc114ad76c54a8935a91f1c2c031c8
Carrot Lemon <trevorkim3141@gmail.com>
2024-11-29 11:50:44 -0500

    optimized a bitAdd project files.

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ int main() {
 	// Create a random number generator
 	std::mt19937 gen(rd());
 
-	// Define the range for random numbers (e.g., 0.0 to 1.0)
-	std::uniform_real_distribution<> distr(1, 100);
-	
+	//// Define the range for random numbers (e.g., 0.0 to 1.0)
+	//std::uniform_real_distribution<> distr(1, 100);
+
 	// Vector of 100 random numbers
 	vector<int> arr(100);
 	for (int i = 0; i < arr.size(); ++i) {
-		arr[i] = (int)distr(gen);
+		arr[i] = i+1;
 	}
+	shuffle(arr.begin(), arr.end(), gen);
 
 	// Initialize window ----------------------------------------------------------------
 	sf::RenderWindow window(sf::VideoMode(1440, 1080), "SFML works!");
@@ -39,6 +40,17 @@ int main() {
 	const int speed = 1;
 	int i = 0;
 	int j = 0;
+
+	// Rectangle Array
+	vector<sf::RectangleShape> rarr(100);
+	for (int i = 0; i < arr.size(); i++) {
+		sf::RectangleShape arrItem(sf::Vector2f((float)window.getSize().x * 10.0 / 12.0 / arr.size(), (float)window.getSize().y * 8 / 10.0 * (float)arr[i] / 100.0));
+		arrItem.setPosition(sf::Vector2f((float)window.getSize().x / 12.0 + (float)window.getSize().x * 10.0 / 12.0 / arr.size() * i,
+			(float)window.getSize().y * 1.0 / 10.0 + (float)window.getSize().y * 8 / 10.0 * (1 - (float)arr[i] / 100.0)));
+		if (i == j || i == j + 1)
+			arrItem.setFillColor(Color::Green);
+		rarr[i] = arrItem;
+	}
 
 	while (window.isOpen()) {
 		// Get Delta Time
@@ -64,19 +76,49 @@ int main() {
 
 		// Sort Segment
 
-		if (started) {
+		if (started && i < 100) {
 			if (i < arr.size() - 1) {
 
 				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
-				}
 
+					// swap x pos value in rarr
+					float xtemp = rarr[j].getPosition().x;
+					rarr[j].setPosition(sf::Vector2f(rarr[j + 1].getPosition().x, rarr[j].getPosition().y));
+					rarr[j + 1].setPosition(sf::Vector2f(xtemp, rarr[j + 1].getPosition().y));
+
+					// swap arr index in rarr
+					sf::RectangleShape rtemp = rarr[j];
+					rarr[j] = rarr[j+1];
+					rarr[j+1] = rtemp;
+				}
+				// Reset the current rectangle color to White
+				rarr[j].setFillColor(Color::White);
+
+				// Move to the next rectangle
 				j++;
+
+				// Check if the inner loop has finished
 				if (j == arr.size() - i - 1) {
+					// Finalize the last unsorted item in this pass
+					rarr[arr.size() - i - 1].setFillColor(Color::White);
+
+					// Reset `j` for the next pass
 					j = 0;
+
+					// Move to the next outer loop iteration
 					i++;
+
+					// Highlight the next unsorted item (if any) in Green
+					if (i < arr.size() - 1) {
+						rarr[j].setFillColor(Color::Green);
+					}
+				}
+				else {
+					// Highlight the next rectangle in Green
+					rarr[j].setFillColor(Color::Green);
 				}
 			}
 		}
@@ -84,13 +126,9 @@ int main() {
 		// Draw
 		window.clear();
 
-		for (int i = 0; i < arr.size(); i++) {
-			sf::RectangleShape arrItem(sf::Vector2f((float)window.getSize().x * 10.0 / 12.0 / arr.size(), (float)window.getSize().y * 8 / 10.0 * (float)arr[i] / 100.0));
-			arrItem.setPosition(sf::Vector2f((float)window.getSize().x / 12.0 + (float)window.getSize().x * 10.0 / 12.0 / arr.size() * i, 
-				(float)window.getSize().y * 1.0 / 10.0 + (float)window.getSize().y * 8 / 10.0 * (1 - (float)arr[i] / 100.0)));
-			if (i == j || i == j + 1)
-				arrItem.setFillColor(Color::Green);
-			window.draw(sf::RectangleShape(arrItem));
+
+		for (sf::RectangleShape rect : rarr) {
+			window.draw(sf::RectangleShape(rect));
 		}
 
 		if (!started)
